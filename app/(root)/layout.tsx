ef1a2a1be397066c6097a697cf16975cecb2a27a
Shubham-S-Lahase <shubhamlahase@gmail.com>
2024-03-13 15:37:49 +0530

    headre and navigation for desktop and other devices done, sign-in and sign-out functionality added with the clerk

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,17 @@
+import MobileNav from "@/components/shared/mobilenav";
+import Sidebar from "@/components/shared/sidebar";
+import React from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="root">
+      <div className="root-container">
+        <Sidebar/>
+        <MobileNav/>
+        <div className="wrapper">{children}</div>
+      </div>
+    </main>
+  );
+};
+
+export default Layout;
